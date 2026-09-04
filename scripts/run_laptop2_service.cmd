e27d31994922eb290cbeec8ef22b50c4410e6f89
Sr-2006 <Sr-2006@users.noreply.github.com>
2026-09-04 18:37:02 +0530

    Add run_laptop2_service.cmd wrapper

---
+++ scripts/run_laptop2_service.cmd
@@ -0,0 +1,3 @@
+@echo off
+cd /d "C:\Users\Shashank\OneDrive\Desktop\Smart horizon hackathon"
+"C:\Users\Shashank\AppData\Local\Programs\Python\Python313\python.exe" "C:\Users\Shashank\OneDrive\Desktop\Smart horizon hackathon\scripts\laptop2_engine_service.py" --nats-url nats://172.51.154.253:4222
